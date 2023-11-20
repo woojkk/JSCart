2d5f18d6f6111376944ea341f6d6d0f5ca8ca192
woojin Kim <woojkk@naver.com>
2023-11-20 23:36:36 +0900

    Initial commit

---
+++ src/main/java/com/example/jscart/JsCartApplication.java
@@ -0,0 +1,13 @@
+package com.example.jscart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsCartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JsCartApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/jscart/JsCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jscart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsCartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
