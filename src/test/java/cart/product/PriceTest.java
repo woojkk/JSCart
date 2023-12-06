d9ab1a70eb9bc9bc4f11e1535af19341f56b1832
woojin Kim <woojkk@naver.com>
2023-12-06 21:36:35 +0900

    [test] - 상품 가격 테스트

---
+++ src/test/java/cart/product/PriceTest.java
@@ -0,0 +1,19 @@
+package cart.product;
+
+import cart.product.domain.vo.Price;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PriceTest {
+
+  @Test
+  void failPriceValueUnderZero() {
+    int price = -1;
+
+    Assertions.assertThatThrownBy(() -> {
+      new Price(price);
+    }).isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("가격은 0원 이하일 수 없습니다.");
+  }
+
+}
